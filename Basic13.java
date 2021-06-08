7e8d90b5a82b7195777f19406f36aa61c5de79be
shawnljohnson1980 <shawnljohnson1980@gmail.com>
2021-06-08 19:16:19 -0400

    Basic 13 assignment work

---
+++ Basic13.java
@@ -0,0 +1,95 @@
+public class Basic13 {
+    
+    
+    // public static void printNum(){
+    //     for(int i = 0; i < 256; i++){
+    //          System.out.println(i);
+    //     }
+    // }
+
+    // public static void printSum(){
+    //     int sum = 0;
+    //     for(int i = 0; i < 256; i++){
+    //       sum += i;
+    //          System.out.println ( sum);
+    //     }
+    // }
+    // public static void iterateArray(int[] arr){
+    //     for(int i=0; i<arr.length; i++){
+    //         System.out.println(arr[i]);
+    //     }
+    // }
+    // public static void oddArr(int [] arr){
+    //     for(int i = 0; i < arr.length; i++){
+    //         if(arr[i] % 2 == 1){
+    //             System.out.println(arr[i]);
+    //         }
+    //     }
+    // }
+//     public static void  greaterThanY(int []arr){
+//         int y = 15;
+//             for(int i = 0; i < arr.length; i++){
+//                 if(arr[i] > y){
+//             System.out.println(arr[i]);
+//         }else{
+//             System.out.println("Too Low");
+//         }
+//     }
+// }
+// public static void lessThan(int[] arr){
+//     for(int i = 0; i < arr.length; i++){
+//         arr[i] += arr[i];
+//         if((int)arr[i] <= 0){
+//         System.out.println(arr[i]);
+//     }
+// }
+// }
+// public static void findminval(int [] arr){
+    // for(int i = 0; i < arr.length; i++){
+        // if(arr[i]>minval){
+            // minval = arr[i];
+        // }
+    // }
+// }
+// public static void findMaxval(int []arr){
+    // for(int i = 0; i < arr.length; i++){
+        // if(arr[i]>maxval){
+            // maxval = arr[i];
+        // }
+    // }
+// }
+// public static void findAvg(int [] arr){             
+//     int sum = 0;
+//     for(int i = 0; i < arr.length; i++){
+//         sum = num++;
+//         avgVal = sum/ arr.length-1;
+//         System.out.println(avgVal);                                                                                                                                                                        
+    }
+}
+    public static void maxMinAndAvg(int[] arr){
+        //     //create an array to store the min and avg in --> since min will be an integer and avg will be a double, this array will be mixed 
+            // ArrayList<Object> result = new ArrayList<Object>();
+            // int minval = Basic13.findMin(arr);
+            // double avgVal = Basic13.findAvg(arr);
+            // int maxval = Basic13.findMaxval(arr);
+            // result.add(minval,maxval,avgVal);
+            // System.out.println(result);
+        }
+    }
+public static void main(String[] args){
+    // Basic13.printNum();
+    // Basic13.printSum();
+    int []arr = {4, 12, 18, 35, 22, 9, 45, 10,11, 23, 19};
+    // int []newArr = {-2,5,10,15,20};
+    // Basic13.iterateArray( arr);
+    // Basic13.oddArr(arr);
+    // Basic13.greaterThanY(arr);
+    // Basic13.lessThan(newArr);
+    // Basic13.maxMinAndAvg(arr);
+    // Basic13.findAvg(arr);
+    // Basic13.findMaxval(arr);
+    // Basic13.findMin(arr);
+}
+}
+
+
